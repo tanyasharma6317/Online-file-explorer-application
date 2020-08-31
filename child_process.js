6916d76c3aa0a34a90548fab83b1f59143a9decc
Tanya Sharma <55737296+tanyasharma6317@users.noreply.github.com>
2020-08-31 21:03:16 +0530

    Delete child_process.js

--- child_process.js
+++
@@ -1,8 +0,0 @@
-const {execSync} = require('child_process');
-
-try{
-    const result = execSync(`du -sh "/Users/devisland/Desktop/Node/Node\ Project\ -\ File\ Explorer"`).toString();
-    console.log(result);
-}catch(err){
-    console.log(`Error: ${err}`);
-}
